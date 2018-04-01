d3a0085c6fac8f63facea86e9e2cb90d4bfb44fe
smajida <s.majid.azimi@gmail.com>
2018-04-01 18:59:41 +0200

    added login correct

--- extractTinderProfiles.py
+++ extractTinderProfiles.py
@@ -21,9 +21,9 @@ images = []
 def gettoken_id(fb_username, fb_password):
     global _id
     # وارد کنید  facebook email
-    fb_username = '@gmail.com'
+    #fb_username = '@gmail.com'
 #  وارد کنید facebook password
-    fb_password = ''
+    #fb_password = ''
 
     fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
     fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
